dc9e719da9722dffc29b6f7e431081a824806cf7
Steven Hicks <steven.hicks@camunda.com>
2024-05-24 10:28:31 -0500

    initial commit

---
+++ src/main/java/com/stinky_pants/handle_payment/HandlePaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.stinky_pants.handle_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlePaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlePaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stinky_pants/handle_payment/HandlePaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stinky_pants.handle_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlePaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
