787ba0ca39e42f3c1acda4c240bfcaa4858be04a
Steven Hicks <steven.hicks@camunda.com>
2024-05-24 11:00:53 -0500

    add a worker

---
+++ src/main/java/com/stinky_pants/handle_payment/ChargeCreditCardWorker.java
@@ -0,0 +1,26 @@
+package com.stinky_pants.handle_payment;
+
+import java.util.Map;
+
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.spring.client.annotation.JobWorker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChargeCreditCardWorker {
+  private final static Logger LOG = LoggerFactory.getLogger(ChargeCreditCardWorker.class);
+
+  @JobWorker(type = "charge-credit-card")
+  public Map<String, Integer> chargeCreditCard(final ActivatedJob job) {
+
+    final int totalWithTax = (Integer) job.getVariablesAsMap().get("totalWithTax");
+
+    // Pretend we're actually charging the credit card here
+    LOG.info("charging credit card: {}", totalWithTax);
+
+    return Map.of("amountCharged", totalWithTax);
+  }
+}
