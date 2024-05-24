9b82cd98d8b3fed6fba99a8486c64928a12d2801
Steven Hicks <steven.hicks@camunda.com>
2024-05-24 11:11:45 -0500

    start a process instance on startup

--- src/main/java/com/stinky_pants/handle_payment/HandlePaymentsApplication.java
+++ src/main/java/com/stinky_pants/handle_payment/HandlePaymentsApplication.java
@@ -1,13 +1,38 @@
 package com.stinky_pants.handle_payment;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.camunda.zeebe.client.ZeebeClient;
+
 @SpringBootApplication
-public class HandlePaymentsApplication {
+public class HandlePaymentsApplication implements CommandLineRunner {
+
+	@Autowired
+	private ZeebeClient zeebeClient;
+
+	private final static Logger LOG = LoggerFactory.getLogger(HandlePaymentsApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(HandlePaymentsApplication.class, args);
+	}
+
+	@Override
+	public void run(final String... args) {
+		var processDefinitionKey = "handle-payment-final";
+		var event = zeebeClient.newCreateInstanceCommand()
+				.bpmnProcessId(processDefinitionKey)
+				.latestVersion()
+				.variables(Map.of("total", 100))
+				.send()
+				.join();
+		LOG.info(String.format("started a process: %d", event.getProcessInstanceKey()));
 	}
 
 }
