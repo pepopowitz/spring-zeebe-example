f95743e592a0b7ec86b63ad39cfb98b1c9972009
Steven Hicks <steven.hicks@camunda.com>
2024-05-24 15:05:55 -0500

    remove unused argument

--- src/main/java/com/stinky_pants/handle_payment/ChargeCreditCardWorker.java
+++ src/main/java/com/stinky_pants/handle_payment/ChargeCreditCardWorker.java
@@ -2,7 +2,6 @@ package com.stinky_pants.handle_payment;
 
 import java.util.Map;
 
-import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import io.camunda.zeebe.spring.client.annotation.Variable;
 
@@ -15,8 +14,7 @@ public class ChargeCreditCardWorker {
   private final static Logger LOG = LoggerFactory.getLogger(ChargeCreditCardWorker.class);
 
   @JobWorker(type = "charge-credit-card")
-  public Map<String, Double> chargeCreditCard(final ActivatedJob job,
-      @Variable(name = "totalWithTax") Double totalWithTax) {
+  public Map<String, Double> chargeCreditCard(@Variable(name = "totalWithTax") Double totalWithTax) {
 
     // Pretend we're actually charging the credit card here
     LOG.info("charging credit card: {}", totalWithTax);
