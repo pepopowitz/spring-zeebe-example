4c0fe9710c2c6f41a8cc7136feae59f49c53aafe
Steven Hicks <steven.hicks@camunda.com>
2024-05-24 14:32:43 -0500

    remove noise from application by tapping @PostConstruct instead of implementing CommandLineRunner

--- src/main/java/com/stinky_pants/handle_payment/HandlePaymentsApplication.java
+++ src/main/java/com/stinky_pants/handle_payment/HandlePaymentsApplication.java
@@ -5,16 +5,16 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @Deployment(resources = "classpath:handle-payment.bpmn")
-public class HandlePaymentsApplication implements CommandLineRunner {
+public class HandlePaymentsApplication {
 
 	@Autowired
 	private ZeebeClient zeebeClient;
@@ -25,8 +25,8 @@ public class HandlePaymentsApplication implements CommandLineRunner {
 		SpringApplication.run(HandlePaymentsApplication.class, args);
 	}
 
-	@Override
-	public void run(final String... args) {
+	@PostConstruct
+	public void init() {
 		var processDefinitionKey = "handle-payment-final";
 		var event = zeebeClient.newCreateInstanceCommand()
 				.bpmnProcessId(processDefinitionKey)
